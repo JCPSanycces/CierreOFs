ff1bdeddf44dd99866a8d8ad47138eec26fcc511
JCPSanycces <jl.chiva@sanycces.es>
2026-07-20 15:14:25 +0200

    Cambio el esquema a SANYCCES

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DB_SERVER = os.getenv("DB_SERVER")
 DB_DATABASE = os.getenv("DB_DATABASE")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-SQL_SCHEMA = os.getenv("SQL_SCHEMA", "PROTO")
+SQL_SCHEMA = os.getenv("SQL_SCHEMA", "SANYCCES")
 SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "clave-por-defecto")
 SAGE_TECH_USER = os.getenv("SAGE_TECH_USER", "WEBAPP")
 
